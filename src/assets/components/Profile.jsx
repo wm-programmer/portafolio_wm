954aced271774c5059267f740253492a77f5765b
Willian Muzo <thinxtrasigfox@gmail.com>
2024-02-06 09:02:33 -0500

    despliegue 3

--- src/assets/components/Profile.jsx
+++ src/assets/components/Profile.jsx
@@ -8,9 +8,10 @@ export const Profile = () => {
                 <div className="profile-info">
                     <h1 className="profile-title">Hola soy, <br /> William Muzo</h1>
                     <p className="profile-details">
-                        Ingeniero en Electronica y Telecomunicaciones, apasionado por la programación
-                        y la tecnología, con el deseo de aprender sobre nuevas tecnologías. Capaz de afrontar
-                        nuevos retos para demostrar mis conocimientos y habilidades.
+                    Profesional en Electrónica y Telecomunicaciones, centrado en el desarrollo y aplicación de soluciones tecnológicas. Mi enfoque proactivo e innovador, así como mi responsabilidad, se manifiestan en mi trabajo, especialmente cuando son aplicados a proyectos desafiantes. Poseo habilidades técnicas y una habilidad innata para construir relaciones interpersonales efectivas en equipos multidisciplinarios.
+                    
+                    Mi objetivo es formar parte de un equipo de trabajo dinámico que no solo valore mis conocimientos, sino que también proporcione un entorno propicio para mi crecimiento personal y profesional. Estoy entusiasmado por contribuir a desafíos tecnológicos en constante evolución, al tiempo que desarrollo y amplío mis habilidades en un entorno colaborativo.
+
                     </p>
                 </div>
                 <span className="profile-img-section">
